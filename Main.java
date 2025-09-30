42878f3694317bd41eecf8bb3b0a69e3eeca75a8
Dhruv Torney <148287814+Dhruv0T@users.noreply.github.com>
2025-09-29 20:41:29 -0700

    Main method partially completed - Dhruv

--- Main.java
+++ Main.java
@@ -2,13 +2,23 @@ import java.util.Locale;
 import javax.speech.Central;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         //TODO: add the main algorithm
         //eventually we can speak the madlibs yay so fun!
         // SpeakText(xxx)
+        Scanner scanner = new Scanner(System.in);
+        String emptysentence = "Earlier today, during CSA I decided that I finished my program while watcthing a <noun> <verb>, and I thought it made Mr.Baez feel very <adjective>.";
+        System.out.println("noun: ");
+        String noun = scanner.nextLine();
+        System.out.println("verb: ");
+        String verb = scanner.nextLine();
+        System.out.println("adjective: ");
+        String adjective = scanner.nextLine();
+        String finalsentence = "Earlier today, during CSA I decided that I finished my program while watcthing a " + noun + " " + verb + ", and I thought it made Mr.Baez feel very " + adjective + ".";
+        System.out.println(finalsentence);
     }
 
     public static void SpeakText(String text) {
